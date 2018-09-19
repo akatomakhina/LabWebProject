f4b1aed2e39e147530f5bd929be87ea989595652
Anastasiya Katomakhina <katomakhinaanastasiya@gmail.com>
2018-09-19 12:22:54 +0300

    Injector added.

--- WebProject/GamesProject.DataAccess/InjectorPackage.cs
+++ WebProject/GamesProject.DataAccess/InjectorPackage.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GamesProject.DataAccess.Common.Repositories;
+using GamesProject.DataAccess.Repositories;
+using SimpleInjector;
+using SimpleInjector.Packaging;
 
 namespace GamesProject.DataAccess
 {
     class InjectorPackage : IPackage
     {
+        public void RegisterServices(Container container)
+        {
+            container.Register<IChannelRepository, ChannelDbRepository>();
+            container.Register<IFavoriteGamesRepository, FavoriteGamesDbRepository>();
+            container.Register<IUserRepository, UserDbRepository>();
+            container.Register<IGameRepository, GameDbRepository>();
+        }
     }
 }
